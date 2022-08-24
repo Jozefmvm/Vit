8cfac17b260932cb2d71cab1762138f8e68f574d
Vitali Murovei <murvitalymvm@gmail.com>
2022-08-24 17:32:50 +0300

    Changing with butiful

--- Vit/Final_project/pageObjects/ProductPage.js
+++ Vit/Final_project/pageObjects/ProductPage.js
@@ -4,27 +4,27 @@ const { BasePage } = require('./BasePage');
 class ProductPage extends BasePage{
 
     get greenSamsungS20Fe(){
-        return $('//*[contains(@href, "/product/smartfon-samsung-galaxy-s20-fe-6-128-gb-zelenyy-608491792")]')
+        return $('//*[contains(@href, "/product/smartfon-samsung-galaxy-s20-fe-6-128-gb-zelenyy-608491792")]');
     }
 
     get blueSamsungS20Fe(){
-        return $('//*[contains(@href, "/product/smartfon-samsung-galaxy-s20-fe-6-128-gb-siniy-660358132")]')
+        return $('//*[contains(@href, "/product/smartfon-samsung-galaxy-s20-fe-6-128-gb-siniy-660358132")]');
     }
 
     get riekerShoes(){
-        return $('//*[contains(@href, "/product/kedy-rieker-budte-zdorovy-523866151/")]')
+        return $('//*[contains(@href, "/product/kedy-rieker-budte-zdorovy-523866151/")]');
     }
 
     get xiaomiThermometr(){
-        return $('//*[contains(@href, "/product/beskontaktnyy-infrakrasnyy-termometr-xiaomi-ihealth-214570350/")]')
+        return $('//*[contains(@href, "/product/beskontaktnyy-infrakrasnyy-termometr-xiaomi-ihealth-214570350/")]');
     }
 
     get addTobusket(){
-        return $('.v4k.ui-a.ui-e7 .ui-a1.ui-e7 .ui-a3.ui-a7.ui-d1.ui-e7')
+        return $('.v4k.ui-a.ui-e7 .ui-a1.ui-e7 .ui-a3.ui-a7.ui-d1.ui-e7');
     }
 
     get transitionToBusket(){
-        return $('//*[contains(text(), "В корзине")]/*[contains(text(), "Перейти")]')
+        return $('//*[contains(text(), "В корзине")]/*[contains(text(), "Перейти")]');
     }
 
     async addItem(element){
